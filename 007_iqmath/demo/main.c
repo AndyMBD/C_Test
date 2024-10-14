2ca1b3e3235a26d8cae172ec099d9bd00f790e68
shun2023 <544361935@qq.com>
2024-10-14 09:22:44 +0800

    add TI iqmath for M0

---
+++ 007_iqmath/demo/main.c
@@ -0,0 +1,35 @@
+#include <windows.h>
+#include "stdint.h"
+#include "stdio.h"
+#include "IQmathLib.h"
+// #include "Ifx_Math_LowPass1st_F16.h"
+uint16_t data;
+int16_t result;
+#define DEBUG_LPF 0
+#define DEBUG_PI 1
+void main()
+{
+    stru_Curr.coef  =   6553;/* 65535*0.1 */
+    stru_Curr.yk_1  =   0;
+    uint16_t  count =   100;
+    uint16_t i;
+    #if DEBUG_LPF 
+    for (i=0; i<count; i++) 
+    {
+        data = 100;
+        result = lowPass_filter(&stru_Curr, data);
+        printf("Data = %d, Result =%d\n", i,result);
+    }
+    #endif
+    #if DEBUG_PI
+    for (i=0; i<count; i++) 
+    {
+        data = count;
+        stru_PIRegulator_Current.wInError=1;
+        result = PIRegulator_Inc(&stru_PIRegulator_Current);;
+        printf("Data = %d, Result =%d\n", data,result);
+    }
+    #endif
+
+    return;
+}
